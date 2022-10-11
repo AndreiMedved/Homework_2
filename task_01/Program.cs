e79070a0f0ecbeb05f4536ac51f5ebf908565ab1
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-11 12:25:18 +0300

    add task_1

---
+++ task_01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task_01 Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+int a = new Random().Next(1, 8);
+
+if (a >= 6)
+{
+    Console.WriteLine(a + " It's hollyday!!!");
+}
+else
+{
+    Console.WriteLine(a + " Work!");
+}
+
