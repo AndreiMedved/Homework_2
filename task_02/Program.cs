566e4d1916ff2d658de0b0918662130824aa0a49
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-11 12:42:31 +0300

    add task_02

---
+++ task_02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Task_02 По двум заданным числам проверять является ли одно квадратом другого
+Console.Write("a = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("b = ");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine("a = b*b");
+}
+else if (b == a * a)
+{
+    Console.WriteLine("b = a*a");
+}
+
+else
+{
+    Console.WriteLine("try again");
+}
