2c73ba35d5211ccb89e2a3cd136302231ed4af26
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-11 13:26:07 +0300

    add task_03

---
+++ task_03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Task_03 Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Write("quarter = ");
+int q = int.Parse(Console.ReadLine());
+
+if (q == 1)
+{
+    Console.WriteLine("X >= 0 and Y >= 0");
+}
+else if (q == 2)
+{
+    Console.WriteLine("X <= 0 and Y >= 0");
+}
+else if (q == 3)
+{
+    Console.WriteLine("X <= 0 and Y <= 0");
+}
+else if (q == 4)
+{
+    Console.WriteLine("X >= 0 and Y <= 0");
+}
+else
+{
+    Console.WriteLine("There isn't quarter");
+}
